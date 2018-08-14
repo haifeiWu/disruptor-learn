433ba43833f9f0d3e2c8a7740d2a7f6795733c8a
haifeiWu <1253242444@qq.com>
2018-08-15 00:17:10 +0800

    update

--- src/main/java/cn/whfroever/disruptor/LongEventConsumer.java
+++ src/main/java/cn/whfroever/disruptor/LongEventConsumer.java
@@ -1,5 +1,11 @@
 package cn.whfroever.disruptor;
 
+/**
+ * 消费者业务逻辑
+ *
+ * @author wuhaifei
+ * @date 2018-08-15
+ */
 public class LongEventConsumer {
     public LongEventConsumer(LongEvent event) {
         // 业务逻辑
